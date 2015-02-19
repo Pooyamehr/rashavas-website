b7b27a945612f89f9a8e16fe1c1f9017a077d504
Farsheed <farsheed@farsheed-ThinkPad-T61p.(none)>
2015-02-19 13:58:41 +0330

    gevent is now default server

--- app.py
+++ app.py
@@ -1,18 +1,27 @@
 #!/usr/bin/env python
-#from gevent import monkey
-#monkey.patch_all()
-from bottle import Bottle, run
-from controllers import example
 
-root = Bottle()
+def getControlers():
+    from bottle import Bottle
+    ## Import your apps here
+    #*************************************************************
+    from controllers import example
+
+    #*************************************************************
+    ##
+    root = Bottle()
+
+
 
 if __name__ == '__main__':
     try:
-        import eventlet
-        server = 'eventlet'
+        from gevent import monkey
+        monkey.patch_all()
+        server = 'gevent'
     except ImportError:
         import gunicorn
         server ='gunicorn'
     finally:
+        from bottle import run
+        getControlers()
         run(host='0.0.0.0', port=5000, server=server,
-            workers=16, debug=False)
+            debug=False)
